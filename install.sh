d27fdcbafb0520d6928b08881f0db08b1af01565
bjgebbie <bjgebbie@gmail.com>
2025-07-27 10:51:24 -0500

    function

--- install.sh
+++ install.sh
@@ -10,6 +10,31 @@ echo "5) waybar"
 echo -n "Enter number(s): "
 read -r input
 
+install_zsh_config() {
+    # this needs git, xz-utils, curl, and gcc
+    cd $HOME
+    echo "Installing zshrc..."
+    sleep 1
+
+    curl -fsSL https://sourceforge.net/projects/zsh/files/zsh/5.9/zsh-5.9.tar.xz/download -o zsh-5.9.tar.xz
+    tar -xf zsh-5.9.tar.xz
+
+    cd zsh-5.9
+    ./configure
+    make
+    make install
+    zsh --version
+
+    cd $HOME
+    RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.zshrc
+    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.p10k.zsh
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --all --no-update-rc
+    zsh
+}
+
 if [[ $input == *0* ]]; then
     echo "al"
 fi
@@ -35,27 +60,4 @@ if [[ $input == *5* ]]; then
 fi
 
 
-install_zsh_config() {
-    # this needs git, xz-utils, curl, and gcc
-    cd $HOME
-    echo "Installing zshrc..."
-    sleep 1
 
-    curl -fsSL https://sourceforge.net/projects/zsh/files/zsh/5.9/zsh-5.9.tar.xz/download -o zsh-5.9.tar.xz
-    tar -xf zsh-5.9.tar.xz
-
-    cd zsh-5.9
-    ./configure
-    make
-    make install
-    zsh --version
-
-    cd $HOME
-    RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.zshrc
-    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.p10k.zsh
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install --all --no-update-rc
-    zsh
-}

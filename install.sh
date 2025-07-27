694f580067f6414b411a0fab18fb75b469248c61
bjgebbie <bjgebbie@gmail.com>
2025-07-27 11:31:15 -0500

    rekmove

--- install.sh
+++ install.sh
@@ -27,25 +27,23 @@ show_progress() {
 install_zsh_config () {
     # this needs git, xz-utils, curl, and gcc
     cd $HOME
-    echo "Installing zshrc..."
-    sleep 1
 
-    curl -fsSL https://sourceforge.net/projects/zsh/files/zsh/5.9/zsh-5.9.tar.xz/download -o zsh-5.9.tar.xz > /dev/null
-    tar -xf zsh-5.9.tar.xz > /dev/null
+    curl -fsSL https://sourceforge.net/projects/zsh/files/zsh/5.9/zsh-5.9.tar.xz/download -o zsh-5.9.tar.xz
+    tar -xf zsh-5.9.tar.xz
 
     cd zsh-5.9
-    ./configure  > /dev/null
-    make > /dev/null
-    make install > /dev/null
+    ./configure
+    make
+    make install
     zsh --version
 
     cd $HOME
-    RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" > /dev/null
-    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.zshrc > /dev/null
-    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.p10k.zsh > /dev/null
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf > /dev/null
-    ~/.fzf/install --all --no-update-rc > /dev/null
+    RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.zshrc
+    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.p10k.zsh
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --all --no-update-rc
     zsh
 }
 

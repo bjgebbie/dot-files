07cc12ffbb4b32ab4337f06c53df9d5ef0492793
bjgebbie <bjgebbie@gmail.com>
2025-07-27 16:15:09 -0500

    tweaks

--- install/kitty.sh
+++ install/kitty.sh
@@ -6,14 +6,19 @@ show_progress() {
 
     while kill -0 "$1" 2>/dev/null; do
         i=$(( (i+1) %4 ))
-        printf "\r[%c] Installing ZSH" "${spin:$i:1}"
+        printf "\r[%c] Installing Kitty" "${spin:$i:1}"
         sleep 0.1
     done
-    printf "\r[✓] ZSH Done!        \n"
+    printf "\r[✓] Kitty Done!        \n"
 }
 
-install_zsh_config () {
-    if [ command -v kitty ]; then
+install_kitty_config () {
+    if [[ command -v kitty ]]; then
         curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
     fi;
+    mkdir -p .config/kitty/
+    curl -o https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 }
+
+install_kitty_config > /dev/null 2>&1 </dev/null &
+show_progress $!
--- install/zsh.sh
+++ install/zsh.sh
@@ -28,8 +28,8 @@ install_zsh_config () {
     cd $HOME
     RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.zshrc
-    curl -O https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.p10k.zsh
+    curl -o https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.zshrc ~/.zshrc
+    curl -o https://raw.githubusercontent.com/bjgebbie/dot-files/refs/heads/master/.p10k.zsh ~/.p10k.zsh
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install --all --no-update-rc
 }

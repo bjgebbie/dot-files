0ef4e149fb2c576f832ed57c7df2bbc7fb36694d
bjgebbie <bjgebbie@gmail.com>
2025-08-03 11:54:45 -0500

    eat stdout

--- install/utils/install-fonts.sh
+++ install/utils/install-fonts.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip > /dev/null
-unzip Hack.zip > /dev/null
+wget -q https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip > /dev/null
+unzip -q Hack.zip > /dev/null
 rm Hack.zip
 
 fc-cache -fv > /dev/null

73d1042bdf107ed0b979d6a86f98d7a2a37a8851
bjgebbie <bjgebbie@gmail.com>
2025-08-05 17:35:25 -0500

    delete stdout for nvim

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,9 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
     version = "^2.0.0",
-    dependencies = {
-        'nvim-java/nvim-java'
-    },
     keys = {
         { "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>",   desc = "Display LSP Code Actions" },
         { "<leader>ce", ":lua vim.diagnostic.open_float()<CR>", desc = "Display LSP Code Errors" },
--- .zshrc
+++ .zshrc
@@ -24,3 +24,7 @@ alias vi="vim"
 bindkey -v
 
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"

48ed145c44b40622612b8aec0d7307330e0f837f
bjgebbie <bjgebbie@gmail.com>
2026-07-31 13:38:44 -0500

    added sidekick group bind

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,27 +1,31 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = function(_, opts)
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
         opts.preset = "modern"
         opts.delay = 0
 
-		local wk = require("which-key")
+        local wk = require("which-key")
 
-		wk.add({
-			{
-				"<leader>l",
-				group = "LSP",
-			},
- 			{
-				"<leader>g",
-				group = "Git",
-			},
-			{
-				"<leader>f",
-				group = "Telescope",
-			},
-		})
+        wk.add({
+            {
+                "<leader>s",
+                group = "Sidekick",
+            },
+            {
+                "<leader>l",
+                group = "LSP",
+            },
+            {
+                "<leader>g",
+                group = "Git",
+            },
+            {
+                "<leader>f",
+                group = "Telescope",
+            },
+        })
 
-		return opts
-	end,
+        return opts
+    end,
 }

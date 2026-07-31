e2f9e83f2eb662cdd2fed133470a591e04fd58bf
bjgebbie <bjgebbie@gmail.com>
2026-07-31 14:45:39 -0500

    better navigation

--- .config/nvim/lua/plugins/sidekick.lua
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -4,12 +4,19 @@ return {
 		"neovim/nvim-lspconfig",
 	},
 	opts = {
+		nes = { enabled = false },
 		cli = {
 			win = {
-				layout = "float",
-				float = {
-					height = 0.9,
-					width = 0.9,
+				-- layout = "float",
+				-- float = {
+				-- 	height = 0.9,
+				-- 	width = 0.9,
+				-- },
+				keys = {
+					nav_left = { "<A-h>", "nav_left", expr = true, desc = "navigate to the left window" },
+					nav_down = { "<A-j>", "nav_down", expr = true, desc = "navigate to the below window" },
+					nav_up = { "<A-k>", "nav_up", expr = true, desc = "navigate to the above window" },
+					nav_right = { "<A-l>", "nav_right", expr = true, desc = "navigate to the right window" },
 				},
 			},
 		},
@@ -23,8 +30,6 @@ return {
 		{
 			"<Esc>",
 			"<cmd>lua require('sidekick.cli').close()<CR>",
-			mode = "n",
-			ft = "sidekick",
 			desc = "Close Sidekick",
 		},
 	},

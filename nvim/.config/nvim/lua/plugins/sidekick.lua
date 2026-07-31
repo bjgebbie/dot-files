5363f94fa418385dd7c8cdd413b8299292c24f47
bjgebbie <bjgebbie@gmail.com>
2026-07-31 14:11:53 -0500

    before updgrad

--- nvim/.config/nvim/lua/plugins/sidekick.lua
+++ nvim/.config/nvim/lua/plugins/sidekick.lua
@@ -3,10 +3,29 @@ return {
 	dependencies = {
 		"neovim/nvim-lspconfig",
 	},
-	opts = {},
+	opts = {
+		cli = {
+			win = {
+				layout = "float",
+				float = {
+					height = 0.9,
+					width = 0.9,
+				},
+			},
+		},
+	},
 	keys = {
-		{ "<leader>sk", "<cmd>Sidekick<CR>", desc = "Toggle Sidekick" },
-		{ "<Esc>", "<cmd>Sidekick<CR>", ft = "sidekick", desc = "Toggle Sidekick" },
-		{ "<C-c>", "<cmd>Sidekick<CR>", ft = "sidekick", desc = "Toggle Sidekick" },
+		{
+			"<leader>sk",
+			"<cmd>lua require('sidekick.cli').toggle({name='copilot',focus=true})<CR>",
+			desc = "Toggle Sidekick",
+		},
+		{
+			"<Esc>",
+			"<cmd>lua require('sidekick.cli').close()<CR>",
+			mode = "n",
+			ft = "sidekick",
+			desc = "Close Sidekick",
+		},
 	},
 }
